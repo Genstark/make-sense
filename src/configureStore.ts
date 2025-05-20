33115c59bbb577d51685e01e0f80e81432b13596
Gautam <76781773+Genstark@users.noreply.github.com>
2025-05-20 16:22:17 +0530

    Update configureStore.ts
    update redux store

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -1,10 +1,9 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './store';
 
-export default function configureStore() {
-    return createStore(
-        rootReducer,
-        // @ts-ignore
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
+export default function setupStore() {
+    return configureStore({
+        reducer: rootReducer,
+        devTools: typeof window !== 'undefined' && (window as any).__REDUX_DEVTOOLS_EXTENSION__,
+    });
 }
